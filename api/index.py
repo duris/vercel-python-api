6ef53bc53ecfc0b7a9a7cd4535a08b9b62db4a89
Ross <ross@duris.io>
2022-10-16 10:37:29 -0400

    load new string

--- api/index.py
+++ api/index.py
@@ -6,6 +6,6 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(str('Hello World!!').encode())
+        self.wfile.write(str('Hello from Python from a Vercel Serverless Function!').encode())
         return
 
